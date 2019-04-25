66a52de92eb8da3bcea0a0d3853963714b7a7a46
nagred01 <nagender.reddy@protiviti.com>
2019-04-24 21:24:03 -0700

    Deleted unwanted changes

--- src/appNavigation/App.js
+++ src/appNavigation/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import {createAppContainer, createStackNavigator} from 'react-navigation';
 import HomeScreen from '../screen/HomeScreen'
 import ListDataScreen from '../screen/ListDataScreen';
-import ItemDetailScreen from '../screen/ItemDetailScreen';
-import TestGestureHandler from "../screen/TestGestureHandler";
-
 type Props = {};
 const MainNavigator = createStackNavigator({
     Home: {screen: HomeScreen},
-    ListData: {screen: ListDataScreen},
-    ItemDetail:{ screen: ItemDetailScreen},
-    TestGestureHandler:{ screen : TestGestureHandler}
+    ListData: {screen: ListDataScreen}
 
 }, {
     headerMode: 'none',
